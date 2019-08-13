07293ca655c35fcd742a92fc57f50a146b035121
asdfry <asdfry@naver.com>
2019-08-13 15:00:41 +0900

    Updates

---
+++ precache-manifest.3870e6c6d83a911e78c5fd02d470b6de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f900c6b71ee26f6859404edf00caaec",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "f6462b04edde089d373d",
+    "url": "/movie_app_2019/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "0412129e6fe003322cd1",
+    "url": "/movie_app_2019/static/js/2.af5468dd.chunk.js"
+  },
+  {
+    "revision": "f6462b04edde089d373d",
+    "url": "/movie_app_2019/static/js/main.14294a7e.chunk.js"
+  },
+  {
+    "revision": "ba3076a0c8d176265049",
+    "url": "/movie_app_2019/static/js/runtime~main.1d21743e.js"
+  }
+]);
